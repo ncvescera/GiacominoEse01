95409c20c4f29eb1efbf1bf69c5b59cd3c39dff2
Nicolo Vescera <nicolo.vescera@gmail.com>
2017-10-05 19:10:52 +0200

    Completato esercizio 5

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
+#define MAX 15
 
 int main(int argc, char** argv) {
+    int numero = 0;
+    short int binary[MAX];
+    short int resto = 0;
+    
+    printf("Inserisci un numero tra 0 e 15: ");
+    scanf("%d",&numero);
+    
+    int tnum = numero;
+    
+    int i = 0;
+    if(numero >= 0 && numero <= 15){
+        do{
+            resto = tnum%2;
+            tnum /= 2;
+            
+            binary[i] = resto;
+           
+            i++;       
+        }while(tnum != 0);
+        
+        for(i ; i>0; i--){
+            printf("%hd",binary[i-1]);
+        }
+    }
+    else{
+        printf("Error! Incorrect number.");
+        return (EXIT_FAILURE);
+    }
+    
     return (EXIT_SUCCESS);
 }
 
